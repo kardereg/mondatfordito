e824dfecaa3cbcbc8361951976bd7ae9927235d4
Istvan Lazar <kardereg@example.com>
2019-03-28 01:07:38 +0100

    start2 and evaluate2 added - one step forward and backward

---
+++ evaluate2.php
@@ -0,0 +1,63 @@
+<?php
+header("Content-Type: text/html; charset=utf-8");
+session_start();
+echo '<html>
+<head>
+    <title>Evaluate</title>
+        <link href="style/eval_style.css" rel="stylesheet" type="text/css" />    
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+<body>
+';
+function renderLine($column1, $column2)
+{
+    echo "<tr>";
+    echo '<td>' . $column1 . '</td>';
+    echo '<td>' . $column2 . '</td>';
+    echo "</tr>";
+}
+/*
+echo 'Hungarian: ' . $_POST["hungarian"] . '<br>';
+echo 'Your solution: ' . $_POST["english"] . '<br>';
+echo 'Correct solution: ' . $_SESSION['row_english'] . '<br>';
+*/
+$row = [];
+$id = 0;
+if ( $_GET["id"] ) {
+    $id = $_GET["id"];
+    if ( isset($_POST["english"]) ) {
+        $posted_english = $_POST["english"];
+    } else {
+        $posted_english = "";
+    }
+    include('connect-db.php');
+    $connection->set_charset("utf8");
+    $result = mysqli_query($connection,"SELECT * FROM mondatok WHERE id = $id");
+    $row = mysqli_fetch_array($result);
+    mysqli_close($connection);
+    
+    echo $row["category"] . "<br>";
+    echo "<table border='1' cellpadding='10'>";
+    renderLine(' Hungarian: ', $row["hungarian"]);
+    renderLine(' Your solution: ', $posted_english);
+    renderLine(' Correct solution: ', $row["english"]);
+    echo "</table>";
+    
+    
+    if ($posted_english === $row['english']) {
+      echo '<div style="color:red;"><b> 100% OK </b><br></div>';
+    }
+    echo '<br>';
+    
+    echo '<input type="submit" name="prev" value="<< Prev" style="height:25px; width:100px" onClick="window.location=\'start2.php?id=' . ($id-1) . '\';"/>';
+    echo '<br>';
+    echo '<br>';
+    echo '<input type="submit" name="next" value="Next >>" style="height:25px; width:100px" onClick="window.location=\'start2.php?id=' . ($id+1) . '\';"/>';
+} else {
+    echo "ERROR...id is missing!";
+}
+echo '
+</body>
+</html>
+';
+?>
---
+++ start2.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+header("Content-Type: text/html; charset=utf-8");
+echo '<html>
+<head>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+<body>
+';
+// connect to the database
+include('connect-db.php');
+$connection->set_charset("utf8");
+if ( $_GET["id"] ) {
+    $id = $_GET["id"];
+    $result = mysqli_query($connection,"SELECT * FROM mondatok WHERE id = $id");
+} else {
+    $result = mysqli_query($connection,"SELECT * FROM mondatok ORDER BY RAND() LIMIT 1");
+}
+$row = mysqli_fetch_array($result);
+mysqli_close($connection);
+$_SESSION['row_english'] = $row['english'];
+echo '
+<b>Translate it to English!</b><br><br>
+<form action="evaluate2.php?id=' . $row['id'] . '" name="confirmationForm" method="post">
+    Hungarian:<br><b><span style="background-color:#ffe0cc;">' . $row['hungarian'] . '</span>
+    <input type="hidden" name="hungarian" value="' . $row['hungarian'] . '" size="40" readonly></br></br>
+    English:<br><textarea id="confirmationText" class="text" cols="40" rows ="0" name="english"></textarea><br><br>
+   <input type="submit" value="Check Solution" class="submitButton" style="height:25px; width:120px">
+</form>
+';
+echo '
+</body>
+</html>
+';
+
+include('connect-db.php');
+$connection->set_charset("utf8");
+$insert_sql = "INSERT INTO history (mondat_id, datetime) VALUES ( " . $row['id'] . ",now())";
+if (!(mysqli_query($connection, $insert_sql))) {
+    echo "Error: " . $insert_sql . "<br>" . mysqli_error($connection);    
+//} else {
+//    echo "New record created successfully";
+}
+mysqli_close($connection);
+
+?>
